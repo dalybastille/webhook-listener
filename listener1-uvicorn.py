a28706950715745b2fc6a31bffb24f67d0705730
dalybastille <130799488+dalybastille@users.noreply.github.com>
2023-04-16 15:57:46 -0600

    Updated lisytener1.-uvicorn.py to include ssl and config.ini.

--- listener1-uvicorn.py
+++ listener1-uvicorn.py
@@ -1,7 +1,17 @@
+import configparser
 import logging
 from fastapi import FastAPI, Request
 
 app = FastAPI()
+
+# read configuration file
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# get values from configuration file
+port = config.getint('webhook', 'port')
+ssl_keyfile = config.get('webhook', 'ssl_keyfile')
+ssl_certfile = config.get('webhook', 'ssl_certfile')
 
 # configure logging
 logging.basicConfig(filename='/var/log/bastille/webhook.log', level=logging.INFO)
@@ -14,4 +24,4 @@ async def webhook(request: Request):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=5000)
+    uvicorn.run(app, host='0.0.0.0', port=port, ssl_keyfile=ssl_keyfile, ssl_certfile=ssl_certfile)
